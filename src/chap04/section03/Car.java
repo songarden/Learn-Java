3bdd5b1573ba7ccc4b5f97dc001efefbc3f25087
garden <gkdlqkql6@naver.com>
2023-01-02 17:36:24 +0900

    230102 - 클래스 호출

--- src/chap04/section03/Car.java
+++ src/chap04/section03/Car.java
@@ -1,12 +1,42 @@
 package chap04.section03;
 
-public class Car {
-    public boolean isCar() {
+class Car{
+    void run(){
+        System.out.println("차가 달립니다.");
+    }
+
+    void printSpeed(int speed) {
+        System.out.println("현재 속도는 " + speed + " 입니다.");
+    }
+
+    boolean isCar() {
         return true;
     }
 
     public static void main(String[] args) {
+        // Car클래스로부터 인스턴스를 생성하세요.
+
         Car car = new Car();
-        System.out.println(car.isCar());
+
+
+        // Car객체의 run 메소드를 호출해보세요.
+
+        car.run();
+
+
+        // Car객체의 printSpeed 메소드를 호출해보세요. (인자로 정수값 10을 넘깁니다)
+
+        car.printSpeed(10);
+
+
+        // Car객체의 isCar 메소드를 호출해보세요. (반환된 값은 boolean타입 변수 result에 저장합니다)
+
+        boolean result = car.isCar();
+
+
+        System.out.println(result);
     }
 }
+
+
+
