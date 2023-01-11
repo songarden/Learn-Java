334d8c91b8e6908af5d35ae439f568db3d131e84
Son Jung Won <gkdlqkql6@naver.com>
2023-01-11 14:48:50 +0900

    230111 인터페이스 static메소드

---
+++ src/CNU_ACA_230111/Ability.java
@@ -0,0 +1,7 @@
+package CNU_ACA_230111;
+
+public interface Ability {
+    static void sayHello(){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/CNU_ACA_230111/StaticTestMain.java
@@ -0,0 +1,7 @@
+package CNU_ACA_230111;
+
+public class StaticTestMain {
+    public static void main(String[] args) {
+        Ability.sayHello();
+    }
+}
